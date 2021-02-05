0e7abb40a68e56e5221081efc1af19f0e8bb9408
Raju Talukder <63764709+Mostafaarju@users.noreply.github.com>
2021-02-05 11:23:19 +0100

    True vs False

---
+++ true-false.js
@@ -0,0 +1,19 @@
+// Falsy: value
+// false
+// 0
+// '"
+// undefined
+// null
+// NaN
+// Truthy
+// '0'
+// ' '
+// []
+// {}
+
+const age = 4;
+if (age > 0) {
+    console.log('Condition is true');
+} else {
+    console.log('Condition is false');
+}
